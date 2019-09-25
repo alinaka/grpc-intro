d62be205917ab9aad5fd5463c05e7bbc39488036
Alina Gotovtceva <alina.gotovtceva@emlid.com>
2019-09-25 17:48:27 +0300

    simple grpc service with python server and c++ client

---
+++ client.cc
@@ -0,0 +1,109 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#include "test.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::ClientReader;
+using grpc::Status;
+using test::TestRequest;
+using test::TestReply;
+using test::Word;
+using test::Letter;
+using test::Test;
+
+class TestClient {
+ public:
+  TestClient(std::shared_ptr<Channel> channel)
+      : stub_(Test::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  void GetLetters(const std::string& body) {
+	  Word word;
+	  Letter letter;
+	  ClientContext context;
+	  word.set_body(body);
+	  std::unique_ptr<ClientReader<Letter> > reader(
+			  stub_->GetLetters(&context, word));
+	  while (reader->Read(&letter)) {
+		  std::cout << "Got new letter "
+					<< letter.character() << std::endl;
+	  }
+	  Status status = reader->Finish();
+	  if (status.ok()) {
+		std::cout << "GetLetters rpc succeeded." << std::endl;
+	  } else {
+		std::cout << "GetLetters rpc failed." << std::endl;
+	  }
+  }
+
+  std::string SendMessage(const std::string& user, std::int32_t& age) {
+    // Data we are sending to the server.
+	TestRequest request;
+    request.set_name(user);
+    request.set_age(age);
+
+    // Container for the data we expect from the server.
+    TestReply reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->SendMessage(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply.message();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return "RPC failed";
+    }
+  }
+
+ private:
+  std::unique_ptr<Test::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint (in this case,
+  // localhost at port 50051). We indicate that the channel isn't authenticated
+  // (use of InsecureChannelCredentials()).
+  TestClient greeter(grpc::CreateChannel(
+      "localhost:50051", grpc::InsecureChannelCredentials()));
+  std::string user("world");
+  std::int32_t age(88);
+  std::string reply = greeter.SendMessage(user, age);
+  std::cout << "Test received: " << reply << std::endl;
+
+  std::string word("grpc");
+  greeter.GetLetters(word); 
+
+  return 0;
+}
